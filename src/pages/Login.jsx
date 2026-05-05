4e5913b83e8bb25ccddba09d746a985428c4c107
xtjktakel4smkmutubdg-cmyk <xtjkta.kel4.smkmutubdg@gmail.com>
2026-05-05 23:16:20 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
         setError('');
         setLoading(true);
         try {
-            const res = await fetch('http://localhost:3000/api/auth/login', {
+            const res = await fetch('https://portal-e-ujian.vercel.app/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
